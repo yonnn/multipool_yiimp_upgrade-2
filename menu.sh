420a47ccfa41da45d83db977b2b7c56012aa3a6c
cryptopool <cryptopool.builders@gmail.com>
2019-06-18 21:19:25 -0400

    menu updates
    menu updates

--- menu.sh
+++ menu.sh
@@ -6,14 +6,17 @@
 source /etc/functions.sh
 cd $HOME/multipool/yiimp_upgrade
 
-RESULT=$(dialog --stdout --title "Ultimate Crypto-Server YiiMP Updater v1.07" --menu "Choose one" -1 65 4 \
+RESULT=$(dialog --stdout --nocancel --default-item 1 --title "Ultimate Crypto-Server YiiMP Updater v1.08" --menu "Choose one" -1 65 7 \
+' ' "- Upgrade Stratum and YiiMP Single Server Installation -" \
 1 "Upgrade YiiMP Single Server Installation" \
+' ' "- Upgrade Stratum Servers Multi Server Installation -" \
 2 "Upgrade Stratum server(s) - Run on each stratum server" \
+' ' "- Upgrade YiiMP Web Server Multi Server Installation -" \
 3 "Upgrade Web Server - Run on web server" \
 4 Exit)
 if [ $RESULT = ]
 then
-exit;
+bash $(basename $0) && exit;
 fi
 
 if [ $RESULT = 1 ]
